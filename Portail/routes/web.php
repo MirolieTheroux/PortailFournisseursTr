04a61e3fcc34873fe84515903c501ff700cb2b00
NicFleurent <143189669+NicFleurent@users.noreply.github.com>
2024-09-10 12:56:47 -0400

    Création du layout et de la page du formulaire

--- Portail/routes/web.php
+++ Portail/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SuppliersController;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', [SuppliersController::class, 'create'])->name('suppliers.create');
