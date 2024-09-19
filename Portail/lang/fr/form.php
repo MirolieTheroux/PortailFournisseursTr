6d191afc060c0b2c92e8671587c9ee2a8df4e09c
MirolieTheroux <mirolie010@gmail.com>
2024-09-19 11:13:36 -0400

    front-end section coordonnées

--- Portail/lang/fr/form.php
+++ Portail/lang/fr/form.php
@@ -11,4 +11,21 @@ return [
     'identificationTitle' => 'Identification',
     'identificationCompanySection' => 'Entreprise',
     'identificationAuthentificationSection' => 'Authentification',
+    'contactDetailsTitle' => 'Coordonnées',
+    'contactDetailsAddressSection' => 'Adresse',
+    'contactDetailsTelNumbersSection' => 'Téléphones',
+    'searchAddress' => 'Entrez votre adresse',
+    'civicNumberLabel' => 'Nᵒ civique',
+    'streetName' => 'Rue',
+    'officeNumber' => 'Bureau',
+    'city' => 'Ville',
+    'province' => 'Province',
+    'region' => 'Région administrative',
+    'postalCode' => 'Code postal',
+    'website' => 'Site internet',
+    'telType' => 'Type',
+    'telNumber' => 'Numéro',
+    'telExtension' => 'Poste',
+    'telNumberList' => 'Liste des # téléphone'
+    
 ];
--- Portail/resources/views/suppliers/create.blade.php
+++ Portail/resources/views/suppliers/create.blade.php
@@ -203,7 +203,131 @@
     <!--LICENCE RBQ-->
     
     <!--COORDONNÉES-->
-    
+    <div class="container bg-white rounded my-2">
+        <div class="row d-none d-md-block">
+            <div class="col-12 rounded-top fond-image fond-coordonnees"></div> <!--TODO::Trouver une autre image de fond-->
+        </div>
+        <div class="row">
+            <div class="d-none d-md-block col-12 text-center">
+                <h1 class="section-title">{{__('form.contactDetailsTitle')}}</h1>
+            </div>
+        </div>
+        <div class="row px-3">
+            <div class="col-12 col-md-6 d-flex flex-column justify-content-between">
+                <h2 class="text-center section-subtitle">{{__('form.contactDetailsAddressSection')}}</h2>
+                <div class="text-center mb-3">
+                    <div class="form-floating ">
+                        <input type="text" name="contactDetails-searchAddress" id="contactDetails-searchAddress" class="form-control" placeholder="">
+                        <label for="contactDetails-searchAddress">{{__('form.searchAddress')}}</label>
+                    </div>
+                </div>
+                <div class="text-center d-flex flex-row mb-3">
+                    <div class="form-floating col-3">
+                        <input type="text" name="contactDetails-civicNumber" id="contactDetails-civicNumber" class="form-control" placeholder="">
+                        <label for="contactDetails-civicNumber">{{__('form.civicNumberLabel')}}</label>
+                    </div>
+                    <div class="form-floating col-6 px-2">
+                        <input type="text" name="contactDetails-streetName" id="contactDetails-streetName" class="form-control" placeholder="">
+                        <label for="contactDetails-streetName">{{__('form.streetName')}}</label>
+                    </div>
+                    <div class="form-floating col-3">
+                        <input type="text" name="contactDetails-officeNumber" id="contactDetails-officeNumber" class="form-control" placeholder="">
+                        <label for="contactDetails-officeNumber">{{__('form.officeNumber')}}</label>
+                    </div>
+                </div>
+                <div class="text-center d-flex flex-row mb-3">
+                    <div class="form-floating col-6 pe-2">
+                        <select name="contactDetails-category" id="contactDetails-city" class="form-select" aria-label="">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            <option value="opel">Opel</option>
+                            <option value="audi">Audi</option>
+                        </select>
+                        <label for="contactDetails-category">{{__('form.city')}}</label>
+                    </div>
+                    <div class="form-floating col-6">
+                        <select name="contactDetails-province" id="contactDetails-province" class="form-select" aria-label="">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            <option value="opel">Opel</option>
+                            <option value="audi">Audi</option>
+                        </select>
+                        <label for="contactDetails-province">{{__('form.province')}}</label>
+                    </div>
+                </div>
+                <div class="text-center d-flex flex-row mb-3">
+                    <div class="form-floating col-8 pe-2">
+                        <select name="contactDetails-region" id="contactDetails-region" class="form-select" aria-label="">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            <option value="opel">Opel</option>
+                            <option value="audi">Audi</option>
+                        </select>
+                        <label for="contactDetails-region">{{__('form.region')}}</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="text" name="contactDetails-postalCode" id="contactDetails-postalCode" class="form-control" placeholder="">
+                        <label for="contactDetails-postalCode">{{__('form.postalCode')}}</label>
+                    </div>
+                </div>  
+                <div class="text-center mb-3">
+                    <div class="form-floating">
+                        <input type="text" name="contactDetails-website" id="contactDetails-website" class="form-control" placeholder="">
+                        <label for="contactDetails-website">{{__('form.website')}}</label>
+                    </div>
+                </div>  
+            </div>
+            <div class="col-12 col-md-6 d-flex flex-column justify-content-between">
+                <h2 class="text-center section-subtitle">{{__('form.contactDetailsTelNumbersSection')}}</h2>
+                <div class="text-center d-flex flex-row mb-3">
+                    <div class="form-floating col-3">
+                        <select name="contactDetails-telType" id="contactDetails-telType" class="form-select" aria-label="">
+                            <option value="bureau">Bureau</option>
+                            <option value="telecopieur">Télécopieur</option>
+                            <option value="cellulaire">Cellulaire</option>
+                        </select>
+                        <label for="contactDetails-telType">{{__('form.telType')}}</label>
+                    </div>
+                    <div class="form-floating col-4 px-2">
+                        <input type="text" name="contactDetails-telNumber" id="contactDetails-telNumber" class="form-control" placeholder="">
+                        <label for="contactDetails-telNumber">{{__('form.telNumber')}}</label>
+                    </div>
+                    <div class="form-floating col-2">
+                        <input type="text" name="contactDetails-telExtension" id="contactDetails-telExtension" class="form-control" placeholder="">
+                        <label for="contactDetails-telExtension">{{__('form.telExtension')}}</label>
+                    </div>
+                    <div class="">
+                        <button type="button" class="m-2 py-1 px-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+                            </svg>
+                        </button><!--TODO::Modifier le bouton-->
+                    </div>
+                </div>
+                <div class="form-floating mb-3"><!--Prenne le reste de la place-->
+                    <div class="form-control" id="contactDetails-telNumberList" >
+                        <div class="row px-3">
+                            <div class="col-12 col-md-12 d-flex flex-column justify-content-between">
+                                <label class="" for="contactDetails-telNumberList">{{__('form.telNumberList')}}</label> 
+                                <div class="row align-items-center">
+                                    <div class="col-3 col-md-4 d-flex flex-column justify-content-start">
+                                        <label class="form-check-label" for="flexCheckDefault">819-999-999</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center mb-3">
+                <button type="button" class="m-2 py-1 px-3 rounded button-darkblue">{{__('global.cancel')}}</button><!--TODO::Mettre un nom significatif au Id-->
+                <button id="test" type="button" class="m-2 py-1 px-3 rounded button-darkblue">{{__('global.next')}}</button><!--TODO::Mettre un nom significatif au Id-->
+            </div>
+        </div>
+    </div> <!--FIN COORDONÉES-->
+
     <!--CONTACT-->
     
     <!--PIÈCES JOINTES-->
