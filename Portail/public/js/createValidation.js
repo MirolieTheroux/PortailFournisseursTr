d2aaddb71d75278a9a8a1efec7f7b50a63375361
scatman2934 <faucher.jeremy0201@gmail.com>
2024-09-26 11:33:28 -0400

    Base de données categories produits et services

--- Portail/public/js/createValidation.js
+++ Portail/public/js/createValidation.js
@@ -213,3 +213,17 @@ function validateIdentificationPasswordConfirmation() {
     
     input.classList.add('was-validated');
 };
+
+function checkedbox(me) {
+  // Get the checkbox
+  var checkBox = me;
+  // Get the output text
+  var text = document.getElementById("selected" + me.id);
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
--- Portail/resources/views/suppliers/create.blade.php
+++ Portail/resources/views/suppliers/create.blade.php
@@ -141,13 +141,77 @@
             <div class="col-12 col-md-4 d-flex flex-column justify-content-between">
                 <h2 class="text-center section-subtitle">{{__('form.productsAndServiceCategories')}}</h2>
                 <div class="text-center">
-                    
                     <div class="form-floating mb-3">
-                        <select name="product-category" id="product-category" class="form-select" aria-label="">
-                            <option value="volvo">Volvo</option>
-                            <option value="saab">Saab</option>
-                            <option value="opel">Opel</option>
-                            <option value="audi">Audi</option>
+                        <select name="product-category" id="product-category" class="form-select" aria-label="Default select example">
+                            <option selected></option>
+                            <optgroup label="Approvissionements">
+                                <option value="G1">G1 - Aérospatiale</option>
+                                <option value="G2">G2 - Matériel de climatisation et de réfrigération</option>
+                                <option value="G3">G3 - Armement</option>
+                                <option value="G4">G4 - Produits et spécialités chimiques</option>
+                                <option value="G5">G5 - Communication, détection et fibres optiques</option>
+                                <option value="G6">G6 - Matériaux de construction</option>
+                                <option value="G7">G7 - Cosmétiques et articles de toilette</option>
+                                <option value="G8">G8 - Matériel et logiciel informatique</option>
+                                <option value="G9">G9 - Entretien d'équipement de bureau et d'informatique</option>
+                                <option value="G10">G10 - Produits électriques et électroniques</option>
+                                <option value="G11">G11 - Énergie</option>
+                                <option value="G12">G12 - Moteurs, turbines, composants et accessoires connexes</option>
+                                <option value="G13">G13 - Produits finis</option>
+                                <option value="G14">G14 - Équipement de lutte contre l'incendie, de sécurité et de protection</option>
+                                <option value="G15">G15 - Alimentation</option>
+                                <option value="G16">G16 - Préparation alimentaire et équipement de service</option>
+                                <option value="G17">G17 - Ameublement</option>
+                                <option value="G18">G18 - Équipement industriel</option>
+                                <option value="G19">G19 - Machinerie et outils</option>
+                                <option value="G20">G20 - Marine</option>
+                                <option value="G21">G21 - Fourniture et équipement médicaux et produits pharmaceutiques</option>
+                                <option value="G22">G22 - Produits divers</option>
+                                <option value="G23">G23 - Matériel de bureau</option>
+                                <option value="G24">G24 - Papeterie et fournitures de bureau</option>
+                                <option value="G25">G25 - Constructions préfabriqués</option>
+                                <option value="G26">G26 - Publications, formulaires et articles en papier</option>
+                                <option value="G27">G27 - Instruments scientifiques</option>
+                                <option value="G28">G28 - Véhicules spéciaux</option>
+                                <option value="G29">G29 - Intégration de systèmes</option>
+                                <option value="G30">G30 - Textiles et vêtements</option>
+                                <option value="G31">G31 - Équipement de transport et pièces de rechange</option>
+                            </optgroup>
+                            <optgroup label="Services">
+                                <option value="S1">S1 - Recherche et développement (R et D)</option>
+                                <option value="S2">S2 - Études spéciales et analyses - (pas R et D)</option>
+                                <option value="S3">S3 - Services d'architecture et d'ingénierie</option>
+                                <option value="S4">S4 - Traitement de l'information et services de télécommunications connexes</option>
+                                <option value="S5">S5 - Services environnementaux</option>
+                                <option value="S6">S6 - Services de ressources naturelles</option>
+                                <option value="S7">S7 - Services de santé et services sociaux</option>
+                                <option value="S8">S8 - Contrôle de la qualité, essais et inspections et services de représentants techniques</option>
+                                <option value="S9">S9 - Entretien, réparation, modification, réfection et installation de biens et d'équipement</option>
+                                <option value="S10">S10 - Services de garde et autres services connexes</option>
+                                <option value="S11">S11 - Services financiers et autres services connexes</option>
+                                <option value="S12">S12 - Exploitation des installations gouvernementales</option>
+                                <option value="S13">S13 - Services de soutien professionnel et administratif et services de soutien à la gestion</option>
+                                <option value="S14">S14 - Services publics</option>
+                                <option value="S15">S15 - Services de communication, de photographie, de cartographie, d'impression et de publication</option>
+                                <option value="S16">S16 - Services pédagogiques et formation</option>
+                                <option value="S17">S17 - Services de transport, de voyage et de déménagement</option>
+                                <option value="S18">S18 - Location à bail / Location d'équipement</option>
+                                <option value="S19">S19 - Location à bail ou location d'installations immobilières</option>
+                            </optgroup>
+                            <optgroup label="Autres natures de contrat">
+                                <option value="C01">C01 - Bâtiments</option>
+                                <option value="C02">C02 - Ouvrages de génie civil</option>
+                                <option value="C03">C03 - Autres travaux de construction</option>
+                            </optgroup>
+                            <optgroup label="Travaux de construction">
+
+                            </optgroup>
+<!--				
+IMM - Vente de biens immeubles	
+MEU1 - Vente de biens meubles	
+C1 - Concession	
+O1 - Indéterminée	
+-->
                         </select>
                         <label for="product-category">{{__('form.productsAndServiceCategoriesList')}}</label>
                     </div>
@@ -170,16 +234,27 @@
                 <div>
                     <div class="form-floating mb-3">
                         <div class="form-control" placeholder="details" id="company-name" style="height: 308px; overflow-x: hidden; overflow-y: auto;">
-                            <div class="form-check">
-                                <div class="row align-items-center">
-                                    <div class="col-1 col-md-1 d-flex flex-column justify-content-between">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <div>
+                                <div class="row align-items-start">
+                                    <div class="col-1 col-md-1 d-flex flex-column justify-content-start">
+                                        <input class="form-check-input" type="checkbox" onclick="checkedbox(this)" id="category1" value="">
                                     </div>
-                                    <div class="col-3 col-md-4 d-flex flex-column justify-content-start">
-                                        <label class="form-check-label" for="flexCheckDefault">05736535</label>
+                                    <div class="col-4 col-md-4 d-flex flex-column justify-content-start">
+                                        <label class="form-check-label" for="category1">05736535</label>
                                     </div>
-                                    <div class="col-3 col-md-7 d-flex flex-column justify-content-start">
-                                        <label class="form-check-label" for="flexCheckDefault">Service d'entretien ménager</label>
+                                    <div class="col-7 col-md-7 d-flex flex-column justify-content-start">
+                                        <label class="form-check-label" for="category1">Service d'entretien ménager</label>
+                                    </div>
+                                </div>
+                                <div class="row align-items-start">
+                                    <div class="col-1 col-md-1 d-flex flex-column justify-content-start">
+                                        <input class="form-check-input" type="checkbox" onclick="checkedbox(this)" id="category2" value="">
+                                    </div>
+                                    <div class="col-4 col-md-4 d-flex flex-column justify-content-start">
+                                        <label class="form-check-label" for="category2">09563559</label>
+                                    </div>
+                                    <div class="col-7 col-md-7 d-flex flex-column justify-content-start">
+                                        <label class="form-check-label" for="category2">Service d'entretien de pelouse</label>
                                     </div>
                                 </div>
                             </div>
@@ -195,7 +270,8 @@
                         <div class="form-control" id="company-name" style="height: 308px; overflow-x: hidden; overflow-y: auto;">
                             <div class="row px-3">
                                 <div class="col-12 col-md-12 d-flex flex-column justify-content-between">
-                                    <label class="" for="flexCheckDefault">Service d'entretien ménager</label>
+                                    <label class="mb-2" id="selectedcategory1" for="category1" style="display:none;">Service d'entretien ménager</label>
+                                    <label class="mb-2" id="selectedcategory2" for="category2" style="display:none;">Service d'entretien de pelouse</label>
                                 </div>
                             </div>
                         </div>
