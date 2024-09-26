e0baa11305f17793f44b1d8132125bc6d1973ef7
NicFleurent <143189669+NicFleurent@users.noreply.github.com>
2024-09-26 08:21:49 -0400

    delete commentaire review RBQ

--- Portail/resources/views/suppliers/create.blade.php
+++ Portail/resources/views/suppliers/create.blade.php
@@ -201,10 +201,6 @@
 
 
     <!--LICENCE RBQ-->
-    <!--REMARQUES::
-    VALIDER_AVEC_MIRO::Est-ce qu'on veut que les catégories soit aussi cliquables ou juste les checkboxes? 
-    VALIDER_AVEC_MIRO::Essayer de mettre la boîte égale à la licence si temps?
-    -->
     <div class="container bg-white rounded my-2">
         <div class="row d-none d-md-block">
             <div class="col-12 rounded-top fond-image fond-rbq"></div>
