1c6bf093ebc4e06fad9e9fef4a950893c16dd948
NicFleurent <fleurent.nicolas@hotmail.com>
2024-09-29 12:19:03 -0400

    correction des ids

--- Portail/public/js/suppliersCreate/contact.js
+++ Portail/public/js/suppliersCreate/contact.js
@@ -9,9 +9,21 @@ document.addEventListener('DOMContentLoaded', function() {
   const addContactButton = document.querySelector('.add-contact');
   addContactButton.addEventListener("click", cloneContact);
 
-  const subtitle = contactsRow.querySelector('#contactSubtitle');
-  subtitle.id = subtitle.id + contactNumber;
-  subtitle.innerHTML = subtitle.innerHTML + contactNumber;
+  const subtitle = contactsRow.querySelector('#contactSubtitle1');
+  const firstnameInput = contactsRow.querySelector('#contactFirstName1');
+  const firstnameLabel = contactsRow.querySelector('#contactFirstNameLabel1');
+  const lastnameInput = contactsRow.querySelector('#contactLastName1');
+  const lastnameLabel = contactsRow.querySelector('#contactLastNameLabel1');
+  const jobInput = contactsRow.querySelector('#contactJob1');
+  const jobLabel = contactsRow.querySelector('#contactJobLabel1');
+  const emailInput = contactsRow.querySelector('#contactEmail1');
+  const emailLabel = contactsRow.querySelector('#contactEmailLabel1');
+  const telTypeInput = contactsRow.querySelector('#contactTelType1');
+  const telTypeLabel = contactsRow.querySelector('#contactTelTypeLabel1');
+  const telnumberInput = contactsRow.querySelector('#contactTelNumber1');
+  const telNumberLabel = contactsRow.querySelector('#contactTelNumberLabel1');
+  const telExtensionInput = contactsRow.querySelector('#contactTelExtension1');
+  const telExtensionLabel = contactsRow.querySelector('#contactTelExtensionLabel1');
 
 
 
@@ -26,9 +38,58 @@ document.addEventListener('DOMContentLoaded', function() {
       newContact.remove();
     });
 
-    const newSubtitle = newContact.querySelector('#' + subtitle.id);
-    newSubtitle.id = newSubtitle.id.slice(0, -1) + contactNumber;
+    const newSubtitle = newContact.querySelector('#' + subtitle.getAttribute("id"));
+    newSubtitle.setAttribute("id", newSubtitle.getAttribute("id").slice(0, -1) + contactNumber);
     newSubtitle.innerHTML = newSubtitle.innerHTML.slice(0, -1) + contactNumber;
+
+    const newFirstnameInput = newContact.querySelector('#'+firstnameInput.getAttribute("id"));
+    newFirstnameInput.setAttribute("id", firstnameInput.getAttribute("id").slice(0, -1) + contactNumber);
+    newFirstnameInput.value = "";
+    const newFirstnameLabel = newContact.querySelector('#'+firstnameLabel.getAttribute("id"));
+    newFirstnameLabel.setAttribute("id", newFirstnameLabel.getAttribute("id").slice(0, -1) + contactNumber);
+    newFirstnameLabel.setAttribute("for", newFirstnameInput.getAttribute("id"));
+  
+    const newlastnameInput = newContact.querySelector('#'+lastnameInput.getAttribute("id"));
+    newlastnameInput.setAttribute("id", newlastnameInput.getAttribute("id").slice(0, -1) + contactNumber);
+    newlastnameInput.value = "";
+    const newlastnameLabel = newContact.querySelector('#'+lastnameLabel.getAttribute("id"));
+    newlastnameLabel.setAttribute("id", newlastnameLabel.getAttribute("id").slice(0, -1) + contactNumber);
+    newlastnameLabel.setAttribute("for", newlastnameInput.getAttribute("id"));
+  
+    const newJobInput = newContact.querySelector('#'+jobInput.getAttribute("id"));
+    newJobInput.setAttribute("id", newJobInput.getAttribute("id").slice(0, -1) + contactNumber);
+    newJobInput.value = "";
+    const newJobLabel = newContact.querySelector('#'+jobLabel.getAttribute("id"));
+    newJobLabel.setAttribute("id", newJobLabel.getAttribute("id").slice(0, -1) + contactNumber);
+    newJobLabel.setAttribute("for", newJobInput.getAttribute("id"));
+  
+    const newEmailInput = newContact.querySelector('#'+emailInput.getAttribute("id"));
+    newEmailInput.setAttribute("id", newEmailInput.getAttribute("id").slice(0, -1) + contactNumber);
+    newEmailInput.value = "";
+    const newEmailLabel = newContact.querySelector('#'+emailLabel.getAttribute("id"));
+    newEmailLabel.setAttribute("id", newEmailLabel.getAttribute("id").slice(0, -1) + contactNumber);
+    newEmailLabel.setAttribute("for", newEmailInput.getAttribute("id"));
+  
+    const newTelTypeInput = newContact.querySelector('#'+telTypeInput.getAttribute("id"));
+    newTelTypeInput.setAttribute("id", newTelTypeInput.getAttribute("id").slice(0, -1) + contactNumber);
+    newTelTypeInput.value = "desktop";
+    const newTelTypeLabel = newContact.querySelector('#'+telTypeLabel.getAttribute("id"));
+    newTelTypeLabel.setAttribute("id", newTelTypeLabel.getAttribute("id").slice(0, -1) + contactNumber);
+    newTelTypeLabel.setAttribute("for", newTelTypeInput.getAttribute("id"));
+  
+    const newTelnumberInput = newContact.querySelector('#'+telnumberInput.getAttribute("id"));
+    newTelnumberInput.setAttribute("id", newTelnumberInput.getAttribute("id").slice(0, -1) + contactNumber);
+    newTelnumberInput.value = "";
+    const newTelNumberLabel = newContact.querySelector('#'+telNumberLabel.getAttribute("id"));
+    newTelNumberLabel.setAttribute("id", newTelNumberLabel.getAttribute("id").slice(0, -1) + contactNumber);
+    newTelNumberLabel.setAttribute("for", newTelnumberInput.getAttribute("id"));
+  
+    const newTelExtensionInput = newContact.querySelector('#'+telExtensionInput.getAttribute("id"));
+    newTelExtensionInput.setAttribute("id", newTelExtensionInput.getAttribute("id").slice(0, -1) + contactNumber);
+    newTelExtensionInput.value = "";
+    const newTelExtensionLabel = newContact.querySelector('#'+telExtensionLabel.getAttribute("id"));
+    newTelExtensionLabel.setAttribute("id", newTelExtensionLabel.getAttribute("id").slice(0, -1) + contactNumber);
+    newTelExtensionLabel.setAttribute("for", newTelExtensionInput.getAttribute("id"));
 
     contactsRow.append(newContact);
   }
--- Portail/resources/views/suppliers/create.blade.php
+++ Portail/resources/views/suppliers/create.blade.php
@@ -571,7 +571,7 @@
         <div id="contactsRow" class="row justify-content-center px-3">
             <div id="referenceContact" class="col-6 d-flex flex-column justify-content-between mb-2">
                 <div class="row">
-                    <h2 id="contactSubtitle" class="col-10 offset-1 text-center section-subtitle">{{__('form.contactsSubtitle')}} #</h2>
+                    <h2 id="contactSubtitle1" class="col-10 offset-1 text-center section-subtitle">{{__('form.contactsSubtitle')}} #1</h2>
                     <button type="button" class="col-1 delete-contact p-0 d-none">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
@@ -583,46 +583,46 @@
                     <div class="row">
                         <div class="col-6 text-center mb-4">
                             <div class="form-floating">
-                                <input type="text" name="contactFirstName[]" id="contactFirstName" class="form-control" placeholder="">
-                                <label for="contact1-firstName">{{__('form.firstNameLabel')}}</label>
+                                <input type="text" name="contactFirstName[]" id="contactFirstName1" class="form-control" placeholder="">
+                                <label id="contactFirstNameLabel1" for="contactFirstName1">{{__('form.firstNameLabel')}}</label>
                             </div>
                         </div>
                         <div class="col-6 text-center mb-4">
                             <div class="form-floating">
-                                <input type="text" name="contactLastName[]" id="contact-lastName" class="form-control" placeholder="">
-                                <label for="contact1-lastName">{{__('form.lastNameLabel')}}</label>
+                                <input type="text" name="contactLastName[]" id="contactLastName1" class="form-control" placeholder="">
+                                <label id="contactLastNameLabel1" for="contactLastName1">{{__('form.lastNameLabel')}}</label>
                             </div>
                         </div>
                     </div>
                     <div class="text-center mb-4">
                         <div class="form-floating">
-                            <input type="text" name="contactJob[]" id="contactJob" class="form-control" placeholder="">
-                            <label for="contactJob">{{__('form.jobLabel')}}</label>
+                            <input type="text" name="contactJob[]" id="contactJob1" class="form-control" placeholder="">
+                            <label id="contactJobLabel1" for="contactJob1">{{__('form.jobLabel')}}</label>
                         </div>
                     </div>  
                     <div class="text-center mb-4">
                         <div class="form-floating">
-                            <input type="text" name="contactEmail[]" id="contactEmail" class="form-control" placeholder="">
-                            <label for="contactEmail">{{__('form.emailLabel')}}</label>
+                            <input type="text" name="contactEmail[]" id="contactEmail1" class="form-control" placeholder="">
+                            <label id="contactEmailLabel1" for="contactEmail1">{{__('form.emailLabel')}}</label>
                         </div>
                     </div>  
                     <h2 class="text-center section-subtitle">{{__('form.contactDetailsTelNumbersSection')}}</h2>
                     <div class="text-center d-flex flex-row mb-4">
                         <div class="form-floating col-3">
-                            <select name="contactTelType[]" id="contactTelType" class="form-select" aria-label="">
+                            <select name="contactTelType[]" id="contactTelType1" class="form-select" aria-label="">
                                 <option value="desktop">Bureau</option>
                                 <option value="fax">Télécopieur</option>
                                 <option value="cellphone">Cellulaire</option>
                             </select>
-                            <label for="contactTelType">{{__('form.telType')}}</label>
+                            <label id="contactTelTypeLabel1" for="contactTelType1">{{__('form.telType')}}</label>
                         </div>
                         <div class="form-floating col-6 px-2">
-                            <input type="text" name="contactTelNumber[]" id="contactTelNumber" class="form-control" placeholder="">
-                            <label class="ms-2" for="contactTelNumber">{{__('form.telNumber')}}</label>
+                            <input type="text" name="contactTelNumber[]" id="contactTelNumber1" class="form-control" placeholder="">
+                            <label id="contactTelNumberLabel1" class="ms-2" for="contactTelNumber1">{{__('form.telNumber')}}</label>
                         </div>
                         <div class="form-floating col-3">
-                            <input type="text" name="contactTelExtension[]" id="contactTelExtension" class="form-control" placeholder="">
-                            <label for="contactTelExtension">{{__('form.telExtension')}}</label>
+                            <input type="text" name="contactTelExtension[]" id="contactTelExtension1" class="form-control" placeholder="">
+                            <label id="contactTelExtensionLabel1" for="contactTelExtension1">{{__('form.telExtension')}}</label>
                         </div>
                     </div>
                 </div>
