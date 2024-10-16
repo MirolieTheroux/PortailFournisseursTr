2de6d59011254e45af9f9058e9f4c7e10688ff99
NicFleurent <fleurent.nicolas@hotmail.com>
2024-10-16 15:44:50 -0400

    Transfert vers fichier de langue

---
+++ Portail/lang/fr/login.php
@@ -0,0 +1,12 @@
+<?php
+
+// lang/en/messages.php
+
+return [
+  'platform' => 'Portail',
+  'suppliers' => 'fournisseurs',
+  'company' => 'Entreprise',
+  'particular' => 'Particulier',
+  'signinLink' => 'Soumettre une demande d\'inscription',
+  'id' => 'Identifiant (NEQ ou Courriel)',
+];
--- Portail/resources/views/suppliers/login.blade.php
+++ Portail/resources/views/suppliers/login.blade.php
@@ -7,21 +7,21 @@
 @section('content')
 <div class="row h-100 bg-image-login">
   <div class="col-6 d-flex align-items-center justify-content-center h-100 bg-grey-transparent">
-    <h1 class="text-white fw-bold">Portail<br>fournisseurs</h1>
+    <h1 class="text-white fw-bold">{{__('login.platform')}}<br>{{__('login.suppliers')}}</h1>
   </div>
   <div class="col-6 d-flex align-items-center justify-content-center h-100 bg-grey-transparent-gradient p-0">
     <div class="row w-100">
       <div class="offset-3 col-6 bg-white rounded-custom">
         <div class="row">
-          <div class="col-6 text-white bg-blue text-center fs-5 py-2 rounded-top-left">Entreprise</div>
-          <div class="col-6 text-white bg-darkblue text-center fs-5 py-2 rounded-top-right">Particulier</div>
+          <div id="company-button" class="col-6 text-white bg-blue text-center fs-5 py-3 rounded-top-left">{{__('login.company')}}</div>
+          <div id="particular-button" class="col-6 text-white bg-darkblue text-center fs-5 py-3 rounded-top-right">{{__('login.particular')}}</div>
         </div>
-        <form class="d-flex flex-column mx-3">
-          <h3 class="mt-3 text-center">Connexion</h3>
+        <h3 class="mt-3 text-center">{{__('global.login')}}</h3>
+        <form id="company-form" class="d-flex flex-column mx-3">
           <div class="text-start">
             <div class="form-floating mb-3">
                 <input type="text" name="neq" id="neq" class="form-control" placeholder="" value="{{ old('neq') }}" maxlength="10">
-                <label for="neq">{{__('form.neqLabelShort')}}</label>
+                <label for="neq">{{__('login.id')}}</label>
                 <div class="invalid-feedback" id="neqInvalidStart" style="display: none;">{{__('validation.starts_with', ['attribute' => 'NEQ', 'values' => '11, 22, 33 ou 88'])}}</div>
                 <div class="invalid-feedback" id="neqInvalidThird" style="display: none;">{{__('form.identificationValidationNEQ3rd')}}</div>
                 <div class="invalid-feedback" id="neqInvalidCharacters" style="display: none;">{{__('form.identificationValidationNEQOnlyDigits')}}</div>
@@ -54,7 +54,7 @@
           </div>
           <div class="row">
             <div class="col-12 d-flex justify-content-center text-center mb-3">
-              <a class="singin-link" href="">Soumettre une demande d'inscription</a>
+              <a class="singin-link" href="">{{__('login.signinLink')}}</a>
             </div>
           </div>
         </form>
