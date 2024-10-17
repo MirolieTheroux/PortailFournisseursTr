007c65ba8c22e98432e628d7bf7b2a03aff0fd77
NicFleurent <fleurent.nicolas@hotmail.com>
2024-10-17 12:23:26 -0400

    responsive fait

--- Portail/lang/fr/login.php
+++ Portail/lang/fr/login.php
@@ -9,6 +9,8 @@ return [
   'particular' => 'Particulier',
   'signinLink' => 'Soumettre une demande d\'inscription',
   'id' => 'Identifiant (NEQ ou Courriel)',
+  'imACompany' => 'Je suis une entreprise',
+  'imAParticular' => 'Je suis un particulier',
 
   'getWrongSupplier' => 'Vous ne pouvez pas consulter la page d\'un autre fournisseur',
   'wrongCredentials' => 'Votre courriel ou mot de passe est invalide'
--- Portail/resources/views/suppliers/login.blade.php
+++ Portail/resources/views/suppliers/login.blade.php
@@ -6,17 +6,19 @@
 
 @section('content')
 <div class="row h-100 bg-image-login">
-  <div class="col-6 d-flex align-items-center justify-content-center h-100 bg-grey-transparent">
+  <div class="col-6 d-none d-md-flex align-items-center justify-content-center h-100 bg-grey-transparent">
     <h1 class="text-white fw-bold">{{__('login.platform')}}<br>{{__('login.suppliers')}}</h1>
   </div>
-  <div class="col-6 d-flex align-items-center justify-content-center h-100 bg-grey-transparent-gradient p-0">
+  <div class="col-12 col-md-6 d-flex align-items-center justify-content-center h-100 bg-grey-transparent-gradient py-3">
     <div class="row w-100">
-      <div class="offset-3 col-6 bg-white rounded-custom">
-        <div class="row">
+      <div class="offset-md-3 col-12 col-md-6 bg-white rounded-custom">
+        <!--TODO::Programer le changement de formulaire selon les réponses des employés de la ville-->
+        <div class="row d-none d-md-flex">
           <div id="company-button" class="col-6 text-white bg-blue text-center fs-5 py-3 rounded-top-left">{{__('login.company')}}</div>
           <div id="particular-button" class="col-6 text-white bg-darkblue text-center fs-5 py-3 rounded-top-right">{{__('login.particular')}}</div>
         </div>
-        <h3 class="mt-3 text-center">{{__('global.login')}}</h3>
+        <h3 class="d-md-none mt-3 text-center">{{__('login.platform')}} {{__('login.suppliers')}}</h3>
+        <h3 class="d-none d-md-block mt-3 text-center">{{__('global.login')}}</h3>
         <form method="post" action="{{route('suppliers.login') }}" id="company-form" class="form d-flex flex-column mx-3">
         @csrf
           <div class="text-start">
@@ -30,14 +32,8 @@
           </div>
           <div class="col-12 d-flex flex-column justify-content-between">
             <div class="form-floating mt-3">
-                <input type="password" name="password" id="password"  class="form-control" placeholder="" value="{{ old('password') }}" maxlength="12">
+                <input type="password" name="password" id="password"  class="form-control" placeholder="" value="{{ old('password') }}">
                 <label for="password">{{__('form.passwordLabel')}}</label>
-                {{-- <div class="invalid-feedback" id="passwordInvalidEmpty" style="display: none;">{{__('validation.required', ['attribute' => 'Mot de passe'])}}</div>
-                <div class="invalid-feedback" id="passwordInvalidAmount" style="display: none;">{{__('form.identificationValidationMDPAmount')}}</div>
-                <div class="invalid-feedback" id="passwordInvalidLowercase" style="display: none;">{{__('form.identificationValidationMDPLowercase')}}</div>
-                <div class="invalid-feedback" id="passwordInvalidUppercase" style="display: none;">{{__('form.identificationValidationMDPUppercase')}}</div>
-                <div class="invalid-feedback" id="passwordInvalidNumber" style="display: none;">{{__('form.identificationValidationMDPDigits')}}</div>
-                <div class="invalid-feedback" id="passwordInvalidSpecial" style="display: none;">{{__('form.identificationValidationMDPSpecial')}}</div> --}}
             </div>
             @if($errors->has('password'))
               <div class="invalid-feedback-custom">{{ $errors->first('password') }}</div>
@@ -51,6 +47,13 @@
           <div class="row">
             <div class="col-12 d-flex justify-content-center text-center mb-3">
               <a class="singin-link" href="{{ route('suppliers.create') }}">{{__('login.signinLink')}}</a>
+            </div>
+          </div>
+          <div class="row d-md-none">
+            <!--TODO::Programer le changement de formulaire selon les réponses des employés de la ville-->
+            <div class="col-12 d-flex justify-content-center text-center mb-3">
+              <a class="singin-link" href="">{{__('login.imAParticular')}}</a>
+              <a class="singin-link d-none" href="">{{__('login.imACompany')}}</a>
             </div>
           </div>
         </form>
