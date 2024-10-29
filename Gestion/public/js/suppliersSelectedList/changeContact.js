b1ccdc1541b4ae29c7aab78a287687d140da595a
NicFleurent <fleurent.nicolas@hotmail.com>
2024-10-29 09:18:58 -0400

    ajout button contact et début programmation

---
+++ Gestion/public/js/suppliersSelectedList/changeContact.js
@@ -0,0 +1,59 @@
+function hideContacts(){
+  const contactsLists = document.getElementsByClassName('contactsList');
+  for(let i = 0 ; i < contactsLists.length ; i++){
+    const contactsArray = contactsLists[i].getElementsByClassName('contactContainer');
+    const parentContainer = contactsLists[i].parentElement;
+    const upButton = parentContainer.querySelector('.contactUpButtom');
+    const downButton = parentContainer.querySelector('.contactDownButtom');
+
+    if(contactsArray.length > 1){
+      for(let j = 1 ; j < contactsArray.length ; j++){
+        contactsArray[j].classList.add('d-none')
+      }
+      disableButton(upButton);
+    }
+    else{
+      disableButton(upButton);
+      disableButton(downButton);
+    }
+  }
+}
+
+function addUpDownListeners(){
+  const buttons = document.getElementsByClassName('contactchangeButton');
+  for(let i = 0 ; i < buttons.length ; i++){
+    buttons[i].addEventListener('click', changeContact);
+  }
+}
+
+function changeContact(event){
+  const buttonContainer = event.target.parentElement;
+  const parentContainer = buttonContainer.parentElement;
+  const contactsList = parentContainer.getElementsByClassName('contactContainer');
+  const contactsArray = Array.from(contactsList);
+  const index = contactsArray.findIndex(x => (!x.classList.contains('d-none')));
+
+  if(event.target.classList.contains('contactUpButtom')){
+    console.log('up');
+  }
+  else{
+    console.log('down');
+  }
+}
+
+function disableButton(button){
+  button.classList.remove('button-darkblue');
+  button.classList.add('button-disabled');
+  button.removeEventListener('click', changeContact);
+}
+
+function enableButton(button){
+  button.classList.remove('button-disabled');
+  button.classList.add('button-darkblue');
+  button.addEventListener('click', changeContact);
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+  addUpDownListeners();
+  hideContacts();
+});
--- Gestion/resources/views/suppliers/selectedList.blade.php
+++ Gestion/resources/views/suppliers/selectedList.blade.php
@@ -10,20 +10,39 @@
         <div class="px-0 fw-bolder">{{$supplier->name}}</div>
       </div>
       <div class="row">
-        <div class="col-6">
-          <div>Courriel : {{$supplier->email}}</div>
+        <div class="col-4">
+          <div>{{__('form.emailLabel')}} : {{$supplier->email}}</div>
           @foreach ($supplier->phoneNumbers as $phoneNumber)
             <div>{{$phoneNumber->type}} {{$phoneNumber->number}} {{$phoneNumber->extension}}</div>
           @endforeach
         </div>
-        <div class="col-6">
-          @foreach ($supplier->contacts as $contact)
-            <div>{{$contact->first_name}} {{$contact->last_name}}, {{$contact->job}}</div>
-            <div>{{$contact->email}}</div>
-            @foreach ($contact->phoneNumbers as $phoneNumber)
-              <div>{{$phoneNumber->type}} {{$phoneNumber->number}} {{$phoneNumber->extension}}</div>
+        <div class="col-4 d-flex flex-row justify-content-between">
+          <div class="contactsList">
+            @foreach ($supplier->contacts as $contact)
+              <div class="contactContainer">
+                <div>{{$contact->first_name}} {{$contact->last_name}}, {{$contact->job}}</div>
+                <div>{{__('form.emailLabel')}} : {{$contact->email}}</div>
+                @foreach ($contact->phoneNumbers as $phoneNumber)
+                  <div>{{$phoneNumber->type}} {{$phoneNumber->number}} {{$phoneNumber->extension}}</div>
+                @endforeach
+              </div>
             @endforeach
-          @endforeach
+          </div>
+          <div class="d-flex flex-column justify-content-center">
+            <button type="button" class="my-2 py-1 px-2 rounded button-darkblue contactchangeButton contactUpButtom">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"/>
+              </svg>
+            </button>
+            <button type="button" class="my-2 py-1 px-2 rounded button-darkblue contactchangeButton contactDownButtom">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div class="col-4">
+          <div>Contacté</div>
         </div>
       </div>
     </div>
@@ -31,4 +50,5 @@
 @endsection
 
 @section('scripts')
+<script src="{{ asset('js/suppliersSelectedList/changeContact.js') }}"></script>
 @endsection
