79109cf23351beb77cf740e26c8ba72828246d22
NicFleurent <fleurent.nicolas@hotmail.com>
2024-11-17 15:41:53 -0500

    Ajout documentation portail

--- Portail/resources/views/documentation/index.blade.php
+++ Portail/resources/views/documentation/index.blade.php
@@ -22,7 +22,7 @@
 <div class="container-fluid d-flex flex-column h-100">
   <div class="row h-100">
     <!--NICE_TO_HAVE::Box-Shadow pour le menu du côté.-->
-    <div class="d-none d-md-block col-2 bg-white h-100 full-viewport sticky-under-navbar d-flex flex-column justify-content-start">
+    <div class="d-none d-lg-flex left-nav shadow-sm col-3 col-xl-2 bg-white h-100 flex-column justify-content-start">
       <div id="login-nav-button" class="text-start rounded px-3 py-1 mt-2 doc-nav-button">{{__('documentation.login')}}</div>
       <div id="signup-nav-button" class="text-start rounded px-3 py-1 mt-2 doc-nav-button">{{__('documentation.signUp')}}</div>
       <div id="home-nav-button" class="text-start rounded px-3 py-1 mt-2 doc-nav-button">{{__('documentation.homePage')}}</div>
@@ -34,14 +34,12 @@
       <h1 class="text-center">{{__('documentation.title')}}</h1>
       <div id="doc-section-login" class="doc-section">
         <h2 class="text-start">{{__('documentation.loginPage')}}</h2>
-        <h3 class="text-start">{{__('documentation.login')}} - {{__('documentation.company')}}</h3>
         <div class="d-flex w-100 justify-content-center mb-3">
           <video width="700" height="400" controls>
-            <source src="{{ asset('video/inscription_acces.mp4') }}" type="video/mp4">
+            <source src="{{ asset('video/connexion.mp4') }}" type="video/mp4">
               {{__('documentation.videoFail')}}
           </video>
         </div>
-        <h3 class="text-start">{{__('documentation.login')}} - {{__('documentation.particular')}}</h3>
       </div>
 
       <div id="doc-section-signup" class="d-none doc-section">
@@ -70,6 +68,47 @@
         <div class="d-flex w-100 justify-content-center mb-3">
           <video width="700" height="400" controls>
             <source src="{{ asset('video/inscription_soumettre.mp4') }}" type="video/mp4">
+              {{__('documentation.videoFail')}}
+          </video>
+        </div>
+      </div>
+
+      <div id="doc-section-home" class="d-none doc-section">
+        <h2 class="text-start">{{__('documentation.homePage')}}</h2>
+        <h3 class="text-start">{{__('documentation.signUpNavigation')}}</h3>
+        <div class="d-flex w-100 justify-content-center mb-3">
+          <video width="700" height="400" controls>
+            <source src="{{ asset('video/accueil_navigation_web.mp4') }}" type="video/mp4">
+          </video>
+        </div>
+        <h3 class="text-start">{{__('documentation.mobileNavigation')}}</h3>
+        <div class="d-flex w-100 justify-content-center mb-3">
+          <video width="250" height="400" controls>
+            <source src="{{ asset('video/accueil_mobile.mp4') }}" type="video/mp4">
+              {{__('documentation.videoFail')}}
+          </video>
+        </div>
+      </div>
+
+      <div id="doc-section-update" class="d-none doc-section">
+        <h2 class="text-start">{{__('documentation.updateAccount')}}</h2>
+        <h3 class="text-start">{{__('documentation.sectionModification')}}</h3>
+        <div class="d-flex w-100 justify-content-center mb-3">
+          <video width="700" height="400" controls>
+            <source src="{{ asset('video/accueil_modification.mp4') }}" type="video/mp4">
+              {{__('documentation.videoFail')}}
+          </video>
+        </div>
+        <h3 class="text-start">{{__('documentation.statusHistoryAcces')}}</h3>
+        <p>Vidéo à venir</p>
+      </div>
+
+      <div id="doc-section-delete" class="d-none doc-section">
+        <h2 class="text-start">{{__('documentation.deleteAccount')}}</h2>
+        <h3 class="text-start">{{__('documentation.deactivateReactivate')}}</h3>
+        <div class="d-flex w-100 justify-content-center mb-3">
+          <video width="700" height="400" controls>
+            <source src="{{ asset('video/accueil_desactivation_web.mp4') }}" type="video/mp4">
               {{__('documentation.videoFail')}}
           </video>
         </div>
