4e99bbacd1fcb539adfed34f164ac037b122a553
NicFleurent <143189669+NicFleurent@users.noreply.github.com>
2024-11-21 08:55:32 -0500

    Correction des middleware et début du transfert des modifications

--- Portail/routes/web.php
+++ Portail/routes/web.php
@@ -15,28 +15,28 @@ Route::get('/', [SuppliersController::class,'home'])->name('suppliers.home');
 Route::get('/login', [SuppliersController::class,'showLogin'])->name('suppliers.showLogin');
 Route::post('/login', [SuppliersController::class,'login'])->name('suppliers.login');
 Route::get('/signin', [SuppliersController::class, 'create'])->name('suppliers.create');
-Route::post('/logout', [SuppliersController::class, 'logout'])->name('suppliers.logout');
+Route::post('/logout', [SuppliersController::class, 'logout'])->name('suppliers.logout')->middleware('auth');
 
 Route::post('/suppliers/checkEmail', [SuppliersController::class, 'checkEmail'])->name('suppliers.checkEmail');
 Route::post('/suppliers/checkNeq', [SuppliersController::class, 'checkNeq'])->name('suppliers.checkNeq');
 
 Route::get('/services', [SuppliersController::class, 'search']);
 Route::post('suppliers', [SuppliersController::class, 'store'])->name('suppliers.store')->middleware(LoggerMiddleware::class);
-Route::get('suppliers/show', [SuppliersController::class, 'show'])->name('suppliers.show');
+Route::get('suppliers/show', [SuppliersController::class, 'show'])->name('suppliers.show')->middleware('auth');
 Route::get('suppliers/home', [SuppliersController::class, 'home'])->name('suppliers.home');
 
 //Modify suppliers informaiton
-Route::patch('/suppliers/update/identification/{supplier}',[SuppliersController::class, 'updateIdentification'])->name('suppliers.updateIdentification');
-Route::post('/suppliers/update/contactDetails/{supplier}',[SuppliersController::class, 'updateContactDetails'])->name('suppliers.updateContactDetails')->middleware(LoggerMiddleware::class);
-Route::get('/suppliers/remove/{supplier}', [SuppliersController::class, 'removeFromList'])->name('suppliers.removeFromList');
-Route::get('/suppliers/reactivate/{supplier}', [SuppliersController::class, 'reactivate'])->name('suppliers.reactivate');
+Route::patch('/suppliers/update/identification/{supplier}',[SuppliersController::class, 'updateIdentification'])->name('suppliers.updateIdentification')->middleware('auth');
+Route::post('/suppliers/update/contactDetails/{supplier}',[SuppliersController::class, 'updateContactDetails'])->name('suppliers.updateContactDetails')->middleware('auth')->middleware(LoggerMiddleware::class);
+Route::get('/suppliers/remove/{supplier}', [SuppliersController::class, 'removeFromList'])->name('suppliers.removeFromList')->middleware('auth');
+Route::get('/suppliers/reactivate/{supplier}', [SuppliersController::class, 'reactivate'])->name('suppliers.reactivate')->middleware('auth');
 
-Route::post('/suppliers/update/contacts/{supplier}',[SuppliersController::class, 'updateContacts'])->name('suppliers.updateContacts');
-Route::post('/suppliers/update/rbq/{supplier}',[SuppliersController::class, 'updateRbq'])->name('suppliers.updateRbq');
-Route::post('/suppliers/update/productsServices/{supplier}',[SuppliersController::class, 'updateProductsServices'])->name('suppliers.updateProductsServices');
-Route::post('/suppliers/update/attachments/{supplier}',[SuppliersController::class, 'updateAttachments'])->name('suppliers.updateAttachments');
-Route::patch('/suppliers/update/finance/{supplier}',[SuppliersController::class, 'updateFinance'])->name('suppliers.updateFinance');
+Route::post('/suppliers/update/contacts/{supplier}',[SuppliersController::class, 'updateContacts'])->name('suppliers.updateContacts')->middleware('auth');
+Route::post('/suppliers/update/rbq/{supplier}',[SuppliersController::class, 'updateRbq'])->name('suppliers.updateRbq')->middleware('auth');
+Route::post('/suppliers/update/productsServices/{supplier}',[SuppliersController::class, 'updateProductsServices'])->name('suppliers.updateProductsServices')->middleware('auth');
+Route::post('/suppliers/update/attachments/{supplier}',[SuppliersController::class, 'updateAttachments'])->name('suppliers.updateAttachments')->middleware('auth');
+Route::patch('/suppliers/update/finance/{supplier}',[SuppliersController::class, 'updateFinance'])->name('suppliers.updateFinance')->middleware('auth');
 
-Route::get('/attachment/{supplier}/{attachment}', [AttachmentsController::class, 'show'])->name('attachments.show');
+Route::get('/attachment/{supplier}/{attachment}', [AttachmentsController::class, 'show'])->name('attachments.show')->middleware('auth');
 
 Route::get('/doc', [DocumentationController::class, 'index'])->name('documentation.index');
