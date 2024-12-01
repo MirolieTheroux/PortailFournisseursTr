40b3b465e9302dc4be6e1dcace32c4ab1376fab5
NicFleurent <fleurent.nicolas@hotmail.com>
2024-11-30 19:59:52 -0500

    ajout doc parametres

--- Gestion/resources/views/documentation/index.blade.php
+++ Gestion/resources/views/documentation/index.blade.php
@@ -118,7 +118,12 @@
 
       <div id="doc-section-parametersManagement" class="d-none doc-section">
         <h2 class="text-start">{{__('documentation.parametersManagement')}}</h2>
-        <p>Vidéo à venir</p>
+        <div class="d-flex w-100 justify-content-center mb-3">
+          <video width="700" height="400" controls>
+            <source src="{{ asset('video/parametres.mp4') }}" type="video/mp4">
+              {{__('documentation.videoFail')}}
+          </video>
+        </div>
       </div>
 
       <div id="doc-section-emailsManagement" class="d-none doc-section">
